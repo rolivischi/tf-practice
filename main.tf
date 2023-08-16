82230d5d1406c3d96354b962f0cc1f8a477de86f
Roland Vischi <roland.vischi@senacor.com>
2023-08-16 14:04:03 +0300

    gcp storage

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "github_repository" "example" {
   description = "This is my new repository created with Terraform"
   visibility  = "private"
 }
+
+resource "google_storage_bucket" "example_bucket" {
+  name     = "rvischi-example-bucket" # must be globally unique
+  location = "EU"                     # or other location
+}
--- provider.tf
+++ provider.tf
@@ -8,6 +8,22 @@ variable "github_token" {
   sensitive = true
 }
 
+variable "gcp_storage_account" {
+  type      = string
+  sensitive = true
+}
+
+variable "project_id" {
+  type      = string
+  sensitive = true
+}
+
+provider "google" {
+  credentials = jsondecode(var.gcp_storage_account)
+  project     = var.project_id
+  region      = "europe-west1"
+}
+
 provider "github" {
   token = var.github_token
   owner = "senacor3"
